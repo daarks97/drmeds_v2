a7104de9f22a1e3d362075c5444137e12291736a
daarks97 <caue_veroneze@hotmail.com>
2025-04-24 14:29:18 +0000

    salvar

--- src/services/studyPlans_v2/index.ts
+++ src/services/studyPlans_v2/index.ts
@@ -5,7 +5,7 @@ import { fetchStudyPlan } from "./fetchStudyPlans";
 import { createStudyPlan } from "./createStudyPlan";
 import { updateStudyPlan } from "./updateStudyPlan";
 import { deleteStudyPlan } from "./deleteStudyPlan";
-import { completeStudyPlanById } from "@/lib/studyPlans_v2/completeStudyPlan";
+import { completeStudyPlanById } from "@/lib/services/studyPlans_v2/completeStudyPlan";
 import { toggleStudyPlanCompletion } from "./toggleStudyPlanCompletion";
 import { toggleStudyPlanDifficulty } from "./toggleStudyPlanDifficulty";
 
