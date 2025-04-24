0e5dd5947d38ff380b78aaacc9de91c3736b42c0
daarks97 <caue_veroneze@hotmail.com>
2025-04-24 08:35:54 -0300

    Update index.ts

--- src/services/studyPlans_v2/index.ts
+++ src/services/studyPlans_v2/index.ts
@@ -1,2 +1,25 @@
-export * from "./studyPlanCoreService";
-export * from "./fetchStudyPlans"; 
+// src/services/studyPlans_v2/index.ts
+
+// Imports individuais para evitar erros de referência
+import { fetchStudyPlans } from "./fetchStudyPlans";
+import { fetchStudyPlan } from "./fetchStudyPlans";
+import { createStudyPlan } from "./createStudyPlan";
+import { updateStudyPlan } from "./updateStudyPlan";
+import { deleteStudyPlan } from "./deleteStudyPlan";
+import { markStudyPlanAsCompleted } from "./completeStudyPlan";
+import { toggleStudyPlanCompletion } from "./toggleStudyPlanCompletion";
+import { toggleStudyPlanDifficulty } from "./toggleStudyPlanDifficulty";
+import { generateStudyPlan } from "./generateStudyPlan";
+
+// Exportação centralizada (ideal pra evitar quebras)
+export {
+  fetchStudyPlans,
+  fetchStudyPlan,
+  createStudyPlan,
+  updateStudyPlan,
+  deleteStudyPlan,
+  markStudyPlanAsCompleted,
+  toggleStudyPlanCompletion,
+  toggleStudyPlanDifficulty,
+  generateStudyPlan,
+};
